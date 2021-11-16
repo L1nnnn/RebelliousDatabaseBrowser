932d78417cedc0522479c54cdfee369b4985844f
cuizhen <cuizhen97@gmail.com>
2021-11-16 21:29:14 +0800

    修改工程目录，分离前后端

---
+++ RebelliousDatabaseLibrary/rebelliousdatabase.cpp
@@ -0,0 +1,5 @@
+#include "rebelliousdatabase.h"
+
+RebelliousDatabase::RebelliousDatabase()
+{
+}
---
+++ RebelliousDatabaseLibrary/rebelliousdatabase.h
@@ -0,0 +1,12 @@
+#ifndef REBELLIOUSDATABASE_H
+#define REBELLIOUSDATABASE_H
+
+#include "RebelliousDatabase_global.h"
+
+class REBELLIOUSDATABASE_EXPORT RebelliousDatabase
+{
+public:
+    RebelliousDatabase();
+};
+
+#endif // REBELLIOUSDATABASE_H
